5290ffdef768af46e1ed58ae4415029b0338bb14
Mustafaakilll <mustafaakil49@gmail.com>
2023-08-29 22:31:22 +0300

    main.rs fixed

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::net::SocketAddr;
 
 use axum::Router;
-use config::Config;
 use hyper::Server;
+
+use config::Config;
+use posts::router::PostRouter;
 use user::router::UserRouter;
 
 mod config;
@@ -15,16 +17,21 @@ async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
 
     let config = Config::new();
+    let config = &config;
 
-    let db = sqlx::PgPool::connect(&config.database_url)
-        .await
-        .expect("Failed to connect to Postgres");
+    let db = sqlx::PgPool::connect(&config.database_url).await?;
 
-    let state = state::AppState { db, config };
+    let state = state::AppState {
+        db,
+        config: config.clone(),
+    };
 
-    let app = Router::new().nest("/user", UserRouter::new_router(state.clone()));
+    let app = Router::new()
+        .nest("/user", UserRouter::new_router(state.clone()))
+        .nest("/posts", PostRouter::new_router(state.clone()));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let port = config.port;
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
 
     Server::bind(&addr)
         .serve(app.into_make_service())
